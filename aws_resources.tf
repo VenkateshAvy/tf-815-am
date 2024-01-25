5f7eb150a3a3761530d812e5804fe48e3be78f3a
VenkateshAvy <venkateshavy123@gmail.com>
2024-01-25 06:46:52 +0000

    vpc ec2 app

--- aws_resources.tf
+++ aws_resources.tf
@@ -1,4 +1,3 @@
-# VPC - Data Center
 resource "aws_vpc" "tf-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -6,30 +5,23 @@ resource "aws_vpc" "tf-vpc" {
   tags = {
     Name = "tf-vpc"
   }
-}
-
-# VPC - Subnet
+} 
 resource "aws_subnet" "tf-subnet" {
   vpc_id     = aws_vpc.tf-vpc.id
   cidr_block = "10.0.1.0/24"
-availability_zone = "us-east-1c"
+availability_zone = "ap-south-1b"
 map_public_ip_on_launch = "true"
-
   tags = {
     Name = "tf-subnet"
   }
-}
-
-# Internet Gateway 
+} 
 resource "aws_internet_gateway" "tf-igw" {
   vpc_id = aws_vpc.tf-vpc.id
 
   tags = {
     Name = "tf-gateway"
   }
-}
-
-# Route Table
+} 
 resource "aws_route_table" "tf-rtb" {
   vpc_id = aws_vpc.tf-vpc.id
 
@@ -38,60 +30,55 @@ resource "aws_route_table" "tf-rtb" {
     gateway_id = aws_internet_gateway.tf-igw.id
   }
 
+
   tags = {
     Name = "tf-route-table"
   }
-}
-
-# Route Table - Subnet Association
+} 
 resource "aws_route_table_association" "tf-rt-sn" {
   subnet_id      = aws_subnet.tf-subnet.id
   route_table_id = aws_route_table.tf-rtb.id
-}
-
-# Secuirty Group
+} 
 resource "aws_security_group" "tf-sg" {
   name        = "allow-tf"
   description = "Allow SSH - HTTP inbound traffic"
   vpc_id      = aws_vpc.tf-vpc.id
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "tf-sg"
   }
 }
-
-# AWS Instance
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-06aa3f7caf3a30282"
+  ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.medium"
-  key_name = "ravi"
+  key_name = "test.newkps"
   subnet_id = aws_subnet.tf-subnet.id
   vpc_security_group_ids = [aws_security_group.tf-sg.id]
   user_data = file("ecomm.sh")
-
   tags = {
     Name = "ecomm-server"
   }
